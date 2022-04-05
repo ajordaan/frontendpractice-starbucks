55df3a68944eb2b4e1871b1124c289b71dfd1397
Andrew Jordaan <andrew.jordaan@platform45.com>
2022-04-05 22:23:56 +0200

    Split sass into files

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
-import './style.sass';
+import './styles/main.sass';
+import './styles/shared.sass';
+
+import './styles/banner.sass';
+import './styles/extras.sass';
+import './styles/getting-started.sass';
+import './styles/hero.sass';
+import './styles/navbar.sass';
+import './styles/reward-tabs.sass';
 
